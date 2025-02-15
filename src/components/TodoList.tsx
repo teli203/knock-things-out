ef08c51dec8c558197189371763672cc34233494
teli203 <teli203@aol.com>
2025-02-14 19:58:34 -0600

    Create interface in todo list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+interface TodoListProps {
+  todos: { id: number; text: string; completed: boolean }[];
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo, deleteTodo }) => {
+  return (
+    <div>
+      <h3>Tasks</h3>
+      {todos.filter(todo => !todo.completed).map(todo => (
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      ))}
+      <h3 className="mt-4">Completed</h3>
+      {todos.filter(todo => todo.completed).map(todo => (
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
