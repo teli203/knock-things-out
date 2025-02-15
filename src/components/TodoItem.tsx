087b628d30c2827d733b6a3481ec17b197fa35d8
teli203 <teli203@aol.com>
2025-02-14 19:58:01 -0600

    Update Todo item file with base layer functions for input of things to do

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface TodoProps {
+  todo: { id: number; text: string; completed: boolean };
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+}
+
+const TodoItem: React.FC<TodoProps> = ({ todo, toggleTodo, deleteTodo }) => {
+  return (
+    <div className="d-flex justify-content-between align-items-center p-2 border rounded bg-light my-2">
+      <span
+        style={{ textDecoration: todo.completed ? "line-through" : "none", cursor: "pointer" }}
+        onClick={() => toggleTodo(todo.id)}
+      >
+        {todo.text}
+      </span>
+      <button className="btn btn-danger btn-sm" onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
